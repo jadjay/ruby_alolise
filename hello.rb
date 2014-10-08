076e9fabf8ef461c61e11b05e43cc1997e301413
Jadjay Alolise <jerome.avond@alolise.org>
2014-10-09 00:27:20 +0200

    Ajout fonction création liste personne

--- hello.rb
+++ hello.rb
@@ -35,3 +35,26 @@ names={
         "Leila" => "Mazouni",
 }
 names.each {|x,y| print "%s %s\n" % [x,y]}
+
+temp='something'
+personnes=[]
+while temp != ""
+	pers={}
+	{
+		'name' => String,
+		'age' => Integer,
+		'sexe' => ['M','F'],
+	}.each do |item,clas|
+		print "Please give %s (%s): " % [item, clas]
+		temp = gets.chomp
+		if temp == ""
+		elsif clas.is_a? Array and clas.include?(temp)
+			pers.store(item, temp)
+		elsif temp.is_a? clas
+			pers.store(item, temp)
+		end
+	end
+	personnes.push(pers) if pers != {}
+end
+
+puts personnes
