f3e328c084b71972985c495ceaa7aa121dce0020
Jadjay Alolise <jerome.avond@alolise.org>
2014-10-14 19:30:32 +0200

    Ajout de l'internationalisation

---
+++ hello2.rb
@@ -0,0 +1,12 @@
+lang = ARGV[0]
+
+hellos = {
+	'fr' => 'Bonjour le monde',
+	'en' => 'Hello world !',
+	'de' => 'Hallo Welt !',
+	'it' => 'Ciao mondo !',
+	'es' => 'Hola mundo !',
+	'np' => 'Salut les connards',
+}
+
+puts hellos[lang]
