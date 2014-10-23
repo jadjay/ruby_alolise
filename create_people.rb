894e1e9064e731397c5253e55d14c35fc3a0542c
Jérôme Avond <javond@adista.fr>
2014-10-23 14:44:57 +0200

    Erreur de débutant : C'est toujours du String qui sort d'un gets

--- create_people.rb
+++ create_people.rb
@@ -1,24 +1,23 @@
 #!/usr/bin/env ruby
 
-def ask_question item, clas
-		print "Please give %s (%s): " % [item, clas]
-		gets.chomp
+def ask_question item, conf
+		print "Please give %s (%s): " % [item, conf[1] ]
+		gets.chomp 
+    #!= "" or conf[1]
 end
 
 # refactored extract method
-def build_people resp, item, clas, pers
-  case clas
-  when clas == String or clas == Integer
-   if resp.class == clas
+def build_people resp, item, conf, pers
+  maclasse, default = conf
+  # OK je viens de comprendre mon erreur
+  # C'est toujours du String !!!!!
+    print "%s %s\n" % [ maclasse,resp.class ]
+  if maclasse== Array and default.include? resp
 			pers.store(item, resp)
-   end
-  when clas.class == Array
-    if clas.include? resp
+  elsif resp.class == maclasse
+    puts maclasse
 			pers.store(item, resp)
-    end
   end
-
-  false
 end
 
 temp='something'
@@ -31,14 +30,22 @@ while temp != ""
   # dont use class for a word
   # the reader is lost
 	{
-		'name' => String,
-		'age' => Integer,
-		'sexe' => ['M','F'],
-	}.each do |item,clas|
-    # rename res
-    temp = ask_question item,clas
+		'name' => [ String, 'john doe' ],
+		'age' => [ Fixnum, '20' ],
+		'sexe' => [ Array, ['M','F'] ],
+	}.each do |item,conf|
+    temp = ask_question item,conf
     # rename method we're on building someone
-    if build_people temp, item, clas, pers
+    if temp
+      build_people temp, item, conf, pers
+    else
+      exit
+    end
+
+    #if not build_people temp, item, clas, pers
+    #  puts "respect the class\n"
+    #  temp = ask_question item,clas
+    #end
 	end
 	personnes.push(pers) if pers != {}
 end
